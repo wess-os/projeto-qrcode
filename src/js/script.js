695cdf64667abe3475951ef9071cabe580b1529c
wes <120646871+werlao@users.noreply.github.com>
2023-05-25 17:08:36 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+const wrapper = document.querySelector('.wrapper'),
+qrInput = wrapper.querySelector('.form input'),
+qrImg = wrapper.querySelector('.qr-code img'),
+generateBtn = wrapper.querySelector('.form button');
+
+generateBtn.addEventListener('click', () => {
+    let qrValue = qrInput.value;
+    if(!qrValue) return;
+    generateBtn.innerText = 'Gerando QRCode...';
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+    qrImg.addEventListener('load', () => {
+        wrapper.classList.add('active');
+        generateBtn.innerText = 'Gerar QRCode';
+    })
+});
+
+qrInput.addEventListener('keyup', () => {
+    if(!qrInput.value) {
+        wrapper.classList.remove('active');
+    }
+});
